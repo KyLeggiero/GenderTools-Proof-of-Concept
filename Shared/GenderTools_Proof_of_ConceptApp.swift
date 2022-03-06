4296436a08e395c129526673e71c60c184cc24a6
Ky Leggiero <KyLeggiero@Gmail.com>
2022-03-05 19:13:39 -0700

    Initial commit

---
+++ Shared/GenderTools_Proof_of_ConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenderTools_Proof_of_ConceptApp.swift
+//  Shared
+//
+//  Created by Ky Leggiero on 3/5/22.
+//
+
+import SwiftUI
+
+@main
+struct GenderTools_Proof_of_ConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
